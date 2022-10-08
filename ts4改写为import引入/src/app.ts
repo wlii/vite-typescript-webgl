0e3a40d2ee1c846a41f0460d3387672939a73937
webDev <413088646@qq.com>
2022-10-08 17:03:23 +0800

    Add files via upload

---
+++ ts4改写为import引入/src/app.ts
@@ -0,0 +1,12 @@
+import { Engine } from './core/engine';
+
+var engine;
+window.onload = function () {
+    engine = new Engine();
+    engine.start()
+}
+
+//window.onresize = function () {
+//  engine.resize();
+//  console.log(1)
+//}
